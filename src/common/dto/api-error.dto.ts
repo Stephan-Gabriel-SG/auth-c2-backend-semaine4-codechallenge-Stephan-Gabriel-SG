796ee989601bce04110da71117827822e93fdb2b
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-07-27 22:05:12 +0300

    add examples

--- src/common/dto/api-error.dto.ts
+++ src/common/dto/api-error.dto.ts
@@ -4,9 +4,15 @@ export class ErrorResponseDto {
   @ApiProperty({ default: false })
   success: boolean;
 
-  @ApiProperty()
+  @ApiProperty({
+    example:
+      'NOT_FOUND_400 | BAD_REQUEST_400 | CONFLICT_409 | INTERNAL_SERVER_ERROR_500',
+  })
   code: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example:
+      'Opération echouée | Livre non disponible | Une erreur est survenue',
+  })
   message: string;
 }
